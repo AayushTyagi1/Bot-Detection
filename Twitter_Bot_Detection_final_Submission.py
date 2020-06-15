befcc72ad121b9456ea067aacc7877d9ce6f42b5
Aayush Tyagi <tyagiaayush5@gmail.com>
2020-06-15 15:21:50 +0530

    Update Twitter_Bot_Detection_final_Submission.py

--- Twitter_Bot_Detection_final_Submission.py
+++ Twitter_Bot_Detection_final_Submission.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
 import seaborn as sns
 import sys
 import warnings
@@ -14,7 +13,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
-import random
+
 
 def Performance(actual_value , predicted_value):
     accuracy = accuracy_score(actual_value , predicted_value) * 100
@@ -178,9 +177,8 @@ cm=confusion_matrix(NB_y,NB_pred)
 Performance(NB_y,NB_pred)
 
 
-
 from sklearn.svm import SVC
-classifier=SVC(kernel='linear', random_state=0)
+classifier=SVC(kernel='rbf', random_state=0)
 classifier.fit(X,y)
 bots = training_data[training_data.bot==1]
 Nbots = training_data[training_data.bot==0]
@@ -202,3 +200,107 @@ NB_pred = classifier.predict(NB)
 from sklearn.metrics import confusion_matrix
 cm=confusion_matrix(NB_y,NB_pred)
 Performance(NB_y,NB_pred)
+
+
+#Best Method - Decision Tree
+from sklearn.tree import DecisionTreeClassifier as DTC
+classifier= DTC(criterion="entropy")
+classifier.fit(X,y)
+test_data = pd.read_csv("test_data_4_students.csv");
+test_data.drop(['id_str', 'screen_name', 'location', 'description', 'url', 'created_at', 'lang', 'status', 'has_extended_profile','name'],axis=1,inplace=True)
+
+
+X1 = training_data.iloc[:, :-1].values
+
+from sklearn.preprocessing import LabelEncoder
+Labelx=LabelEncoder()
+X1[:,6]=Labelx.fit_transform(X1[:,6])
+X1[:,7]=Labelx.fit_transform(X1[:,7])
+X1[:,8]=Labelx.fit_transform(X1[:,8])
+y1_pred = classifier.predict(X1)
+y1_pred=pd.DataFrame(y1_pred);
+y1_pred.to_csv("Result.csv",index=False);
+
+
+My_Own_algorithm
+
+class twitter_bot(object):
+    def __init__(self):
+        pass
+
+    def perform_train_test_split(df):
+        msk = np.random.rand(len(df)) < 0.75
+        train, test = df[msk], df[~msk]
+        X_train, y_train = train, train.iloc[:,-1]
+        X_test, y_test = test, test.iloc[:, -1]
+        return (X_train, y_train, X_test, y_test)
+
+    def bot_prediction_algorithm(df):
+        train_df = df.copy()
+        # converting id to int
+        train_df['id'] = train_df.id.apply(lambda x: int(x))
+        bag_of_words_bot = r'BOT|Bot|bot|b0t|cannabis|tweet me|mishear|follow me|updates every|gorilla|yes_ofc|forget' \
+                           r'expos|kill|clit|bbb|butt|fuck|XXX|sex|truthe|fake|anony|free|virus|funky|RNA|kuck|jargon' \
+                           r'nerd|swag|jack|bang|bonsai|chick|prison|paper|pokem|xx|freak|ffd|dunia|clone|genie|bbb' \
+                           r'ffd|onlyman|emoji|joke|troll|droop|free|every|wow|cheese|yeah|bio|magic|wizard|face'
+
+
+        # converting verified into vectors
+        train_df['verified'] = train_df.verified.apply(lambda x: 1 if ((x == True) or x == 'TRUE') else 0)
+        # check if the name contains bot or screenname contains b0t
+        condition = ((train_df.name.str.contains(bag_of_words_bot, case=False, na=False)) |
+                     (train_df.description.str.contains(bag_of_words_bot, case=False, na=False)) |
+                     (train_df.screen_name.str.contains(bag_of_words_bot, case=False, na=False)) |
+                     (train_df.status.str.contains(bag_of_words_bot, case=False, na=False))
+                     )  # these all are bots
+        predicted_df = train_df[condition]  # these all are bots
+        predicted_df.bot = 1
+        predicted_df = predicted_df[['id', 'bot']]
+
+        # check if the user is verified
+        verified_df = train_df[~condition]
+        condition = (verified_df.verified == 1)  # these all are nonbots
+        predicted_df1 = verified_df[condition][['id', 'bot']]
+        predicted_df1.bot = 0
+        predicted_df = pd.concat([predicted_df, predicted_df1])
+
+        # check if description contains buzzfeed
+        buzzfeed_df = verified_df[~condition]
+        condition = (buzzfeed_df.description.str.contains("buzzfeed", case=False, na=False))  # these all are nonbots
+        predicted_df1 = buzzfeed_df[buzzfeed_df.description.str.contains("buzzfeed", case=False, na=False)][['id', 'bot']]
+        predicted_df1.bot = 0
+        predicted_df = pd.concat([predicted_df, predicted_df1])
+
+        # check if listed_count>16000
+        listed_count_df = buzzfeed_df[~condition]
+        listed_count_df.listed_count = listed_count_df.listed_count.apply(lambda x: 0 if x == 'None' else x)
+        listed_count_df.listed_count = listed_count_df.listed_count.apply(lambda x: int(x))
+        condition = (listed_count_df.listed_count > 16000)  # these all are nonbots
+        predicted_df1 = listed_count_df[condition][['id', 'bot']]
+        predicted_df1.bot = 0
+        predicted_df = pd.concat([predicted_df, predicted_df1])
+        #remaining
+        predicted_df1 = listed_count_df[~condition][['id', 'bot']]
+        predicted_df1.bot = 0 # these all are nonbots
+        predicted_df = pd.concat([predicted_df, predicted_df1])
+        return predicted_df
+
+    def get_predicted_and_true_values(features, target):
+        y_pred, y_true = twitter_bot.bot_prediction_algorithm(features).bot.tolist(), target.tolist()
+        return (y_pred, y_true)
+
+    def get_accuracy_score(df):
+        (X_train, y_train, X_test, y_test) = twitter_bot.perform_train_test_split(df)
+        # predictions on training data
+        y_pred_train, y_true_train = twitter_bot.get_predicted_and_true_values(X_train, y_train)
+        Performance(y_pred_train, y_true_train)
+
+if __name__ == '__main__':
+    train_ = pd.read_csv('training_data_2_csv_UTF.csv')
+    test_ = pd.read_csv('test_data_4_students.csv')
+    
+
+    train_.drop(['id_str', 'location', 'url', 'created_at', 'lang', 'has_extended_profile'],axis=1,inplace=True)
+    test_.drop(['id_str', 'location', 'url', 'created_at', 'lang', 'has_extended_profile'],axis=1,inplace=True)  
+    predicted_df = twitter_bot.bot_prediction_algorithm(test_)   
+    twitter_bot.get_accuracy_score(train_)
